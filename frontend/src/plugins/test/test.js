6129f653200284d88edcb4e9761fbc0ae898e216
Yuriy Senin <troperread@gmail.com>
2022-11-22 18:56:09 +0400

    add test template plugin

---
+++ frontend/src/plugins/test/test.js
@@ -0,0 +1,11 @@
+/*export default {
+    install(Vue, args) {
+        Vue.config.globalProperties.$test = new vAlert;
+
+        /!*
+        Vue.component('my-alert', Alert);
+        Vue.config.globalProperties.$vAlert = function () {
+            /!*this.$emit("showAlert");*!/
+        }*!/
+    }
+}*/
