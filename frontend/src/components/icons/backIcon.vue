a4f349d397855169792ae230f565953d7a40173c
Yuriy Senin <troperread@gmail.com>
2022-11-26 21:06:43 +0400

    add icons

---
+++ frontend/src/components/icons/backIcon.vue
@@ -0,0 +1,10 @@
+<template>
+    <polygon points="352,128.4 319.7,96 160,256 160,256 160,256 319.7,416 352,383.6 224.7,256 "/>
+</template>
+<script>
+export default {
+    name: "backIcon"
+}
+</script>
+<style scoped>
+</style>
