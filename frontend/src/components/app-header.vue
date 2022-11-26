1087a3888b6b8f163b7465eec8a04b366c97c7ae
Yuriy Senin <troperread@gmail.com>
2022-11-26 21:06:32 +0400

    header settings in vuex

--- frontend/src/components/app-header.vue
+++ frontend/src/components/app-header.vue
@@ -1,10 +1,13 @@
 <template>
     <header>
-        <icon class="menuIcon" @click="$emit('openSidebar')">
+        <icon v-if="showSidebarBtn" class="menuIcon" @click="$emit('openSidebar')">
             <menu-icon/>
         </icon>
+        <icon v-if="showBackBtn" class="menuIcon" @click="$emit('openSidebar')" viewBox="65 55 400 400">
+            <back-icon/>
+        </icon>
         <div id="headerTitle">
-            <slot/>
+            {{ headerTitle }}
         </div>
         <icon class="pointsIcon" @click="openMenu">
             <points-icon/>
@@ -17,14 +20,31 @@ import Icon from "@/components/icons/icon";
 import MenuIcon from "@/components/icons/menuIcon";
 import PointsIcon from "@/components/icons/pointsIcon";
 import ActionMenu from "@/components/action-menu";
+import store from "@/store";
+import BackIcon from "@/components/icons/backIcon";
+
 export default {
 	name: "app-header",
-	components: {ActionMenu, PointsIcon, MenuIcon, Icon},
-    data() {
-	    return {
+	components: {BackIcon, ActionMenu, PointsIcon, MenuIcon, Icon},
 
-        }
+    computed: {
+        showSidebarBtn() {
+            return this.$store.getters.showSidebarBtn;
+        },
+        showBackBtn() {
+            return this.$store.getters.showBackBtn;
+        },
+        backBtnUrl() {
+            return this.$store.getters.backBtnUrl;
+        },
+        headerTitle() {
+            return store.getters.headerTitle;
+        },
+        showActionMenu() {
+            return this.$store.getters.showActionMenu;
+        },
     },
+
     methods: {
         openMenu () {
             this.$store.commit("actionMenuStatus", true);
