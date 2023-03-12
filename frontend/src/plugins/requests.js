cc893107f2c7c240216be0dee1f74e7b8cfa0979
Yuriy Senin <troperread@gmail.com>
2023-03-12 13:03:57 +0400

    added request plugin

--- frontend/src/plugins/requests.js
+++ frontend/src/plugins/requests.js
@@ -1,5 +1,49 @@
 const requests = class requests {
 
+    static apiUrl = 'https://jsonplaceholder.typicode.com/1';
+    static baseHeaders = {
+        "Content-Type"  : "application/json",
+        "Accept"        : "application/json",
+    }
+
+    static async sendRequest(url, method, body, headers) {
+        let options = this.getOptions(method, body, headers);
+        return fetch(url, options);
+    }
+
+    static get(url, headers = {}) {
+        return this.sendRequest(url, "GET", {}, headers);
+    }
+
+    static post(url, body = {}, headers = {}) {
+        return this.sendRequest(url, "POST", body, headers);
+    }
+
+    static put(url, body = {}, headers = {}) {
+        return this.sendRequest(url, "PUT", body, headers);
+    }
+
+    static delete(url, body = {}, headers = {}) {
+        return this.sendRequest(url, "DELETE", body, headers);
+    }
+
+
+
+    static getOptions (method, body, headers = {}) {
+        method = method.toUpperCase();
+        let options = {
+            "method": method,
+            "headers": Object.assign(this.baseHeaders, headers),
+        };
+
+        if (method !== "GET" && method !== "HEAD") {
+            options.body = JSON.stringify(body)
+        }
+
+        return options;
+    }
+
+
 }
 
 export default {
