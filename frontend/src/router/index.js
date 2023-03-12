36b5b4669b304764490d9c847f006a63d3a3eda3
Yuriy Senin <troperread@gmail.com>
2023-03-12 19:17:35 +0400

    added a route guards

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -4,40 +4,62 @@ import _Clean from '../views/_Clean.vue';
 import ChatsView from "@/views/ChatsView";
 import store from "@/store";
 
+const ifNotAuthenticated = (to, from, next) => {
+    if (!store.getters.isAuthorized) {
+        next()
+        return
+    }
+    next('/')
+}
+const ifAuthenticated = (to, from, next) => {
+    if (store.getters.isAuthorized) {
+        next()
+        return
+    }
+    next('/login')
+}
+
 const routes = [
     {
         path: '/',
         name: 'Главная',
+        beforeEnter: ifAuthenticated,
         component: ChatsView
     },
     {
         path: '/clean',
         name: 'clean page',
+        beforeEnter: ifAuthenticated,
         component: () => import( '../views/_Clean')
     },
     {
         path: "/chat",
         name: '',
+        beforeEnter: ifAuthenticated,
         component: () => import( '../views/ChatView')
     },
     {
         path: "/login",
         name: '',
+        beforeEnter: ifNotAuthenticated,
         component: () => import( '../views/AuthView')
     },
     {
         path: "/user",
         name: '',
+        beforeEnter: ifAuthenticated,
         component: () => import( '../views/UserProfile')
     },
     {
         path: "/search",
         name: '',
+        beforeEnter: ifAuthenticated,
         component: () => import( '../views/SearchView')
     },
     {
         path: "/settings",
         name: 'Настройки',
+        beforeEnter: ifAuthenticated,
         component: () => import( '../views/SettingsView')
     },
 
