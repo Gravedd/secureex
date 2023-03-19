3ff976cc815cb0fccb5ab7cd4dd7a28210351b25
Yuriy Senin <troperread@gmail.com>
2023-03-19 21:01:27 +0400

    remove router animation

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,6 @@
 <template>
     <router-view v-slot="{ Component }">
-        <transition name="fade">
+        <transition>
             <component :is="Component"/>
         </transition>
     </router-view>
