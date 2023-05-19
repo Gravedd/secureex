ca962eb73b38e05a5b3c6f2524c693d00d625887
Yuriy Senin <troperread@gmail.com>
2023-05-19 21:56:45 +0400

    show user name in chat

--- frontend/src/views/ChatView.vue
+++ frontend/src/views/ChatView.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="appWrapper">
 
-        <app-header>UserName</app-header>
+        <app-header>{{user.name}}</app-header>
 
 
         <div class="componentWrapper">
@@ -27,6 +27,7 @@ import SendIcon from "@/components/icons/sendIcon";
 import store from "@/store"
 import DialogComponent from "@/components/dialogues/Dialog-component";
 import AppHeader from "@/components/headers/app-header";
+import config from "@/config";
 
 export default {
 	name: 'ChatView',
@@ -34,6 +35,7 @@ export default {
     data() {
 	    return {
             with_user: this.$route.params.id,
+            user: {},
         }
     },
     methods: {
@@ -44,10 +46,19 @@ export default {
                 dialogWith: this.with_user
             })
             this.$store.commit("readAllDialogMessagesLocal", this.with_user);
+        },
+        async getUser() {
+            let response = await this.$request.get(config.api + "user/" + this.with_user);
+            if (!response.ok) {
+                return alert("Не удалось получить пользователя");
+            }
+            this.user = await response.json();
+
         }
     },
     beforeMount() {
 	    this.$showLoader();
+	    this.getUser();
         store.dispatch("getMessages", this.with_user).then(() => this.onGetMessages());
     },
     mounted() {
