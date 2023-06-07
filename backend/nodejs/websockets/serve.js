4c0b81549781fa95e9ff430c7918889898d49f84
Yuriy Senin <troperread@gmail.com>
2023-06-07 17:55:53 +0400

    try catch for ws server

--- backend/nodejs/websockets/serve.js
+++ backend/nodejs/websockets/serve.js
@@ -2,7 +2,9 @@ import Server from "./src/components/Server.js";
 import Log from "./src/components/Log.js";
 
 Log.info("Сервер запускается");
-
-let server = new Server();
-
-server.start();
+try {
+    let server = new Server();
+    server.start();
+} catch (Err) {
+    Log.error(Err.message, Err);
+}
